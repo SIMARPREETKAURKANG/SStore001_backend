f4cffa40081da5efeafd2a9d150ea56b2b714fd7
SIMARPREETKAURKANG <skang8442@gmail.com>
2023-08-02 14:55:01 -0400

    first commit

---
+++ app/config/auth.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+    secret: "Simar-secret-key",
+    jwtExpiration: 3600,           // 1 hour
+
+  jwtRefreshExpiration: 86400,   // 24 hours
+
+ 
+
+  /* for test */
+
+  jwtExpiration: 60,          // 1 minute
+
+  // jwtRefreshExpiration: 120,  // 2 minutes
+  };
---
+++ app/config/db.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    
+  url:"mongodb+srv://simarkang77:yHHl4KcxtmluvxYA@cluster.xxh4oyg.mongodb.net/SportStore?retryWrites=true&w=majority"
+  
+  };
